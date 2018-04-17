442f6c9ab31b3a0c59aa8b1d4fc0032f5852e562
Shurui Jia <zmoiynlp@icloud.com>
2018-04-17 15:47:00 +0800

    正在进行ParseTree转AST的工作。 完成了定义语句的大概框架。

--- nfa.py
+++ nfa.py
@@ -16,20 +16,24 @@ table = {
 
 def matchLiteralGeneral(s, start, end, quotation, tag):
     def check(f):
-        def execute(i):
+        def execute():
+            nonlocal i
             if i == end:
                 raise RuntimeError(r'missing terminating %c character' % quotation)
-            yield from f(i)
+            yield from f()
 
         return execute
 
     length = 0
+    i = start + 1
 
     @check
-    def stat1(i):
+    def stat1():
         nonlocal length
+        nonlocal i
         if s[i] == '\\':
-            yield from stat2(i + 1)
+            i += 1
+            yield from stat2()
         elif s[i] == '\n':
             raise RuntimeError(r"'\n' should not be here")
         elif s[i] == quotation:
@@ -37,62 +41,88 @@ def matchLiteralGeneral(s, start, end, quotation, tag):
             return
         else:
             yield s[i]
-            yield from stat1(i + 1)
+            i += 1
+            yield from stat1()
 
     @check
-    def stat2(i):
+    def stat2():
+        nonlocal i
         if s[i] not in escapestart:
             raise RuntimeError(r"unknown escape sequence: '\%s'" % (s[i]))
-        yield from stat5(i)
-        yield from stat4(i)
-        if s[i] == 'x':
-            yield from stat3(i + 1)
+        elif s[i] == 'x':
+            i += 1
+            yield from stat3()
+        elif s[i] in table:
+            yield table[s[i]]
+            i += 1
+            yield from stat1()
+        else:
+            yield from stat4()
 
     @check
-    def stat3(i):
+    def stat3():
+        nonlocal i
         match = hexadecimal.match(s, i)
         if match:
             value = int(match.group(), 16)
             if value >= 256:
                 raise RuntimeError("hex escape sequence out of range")
-            yield match.group()
-            yield from stat1(i + 1)
+            yield chr(value)
+            assert isinstance(match.group(), str)
+            i += len(match.group())
+            yield from stat1()
         else:
             raise RuntimeError(r"\x used with no following hex digits")
 
     @check
-    def stat4(i):
+    def stat4():
+        nonlocal i
         match = octal.match(s, i)
         if match:
             value = int(match.group(), 8)
             if value >= 256:
                 raise RuntimeError("octal escape sequence out of range")
-            yield match.group()
-            yield from stat1(i + 1)
-
-    @check
-    def stat5(i):
-        if s[i] in table:
-            yield table[s[i]]
-        yield from stat1(i + 1)
+            yield chr(value)
+            assert isinstance(match.group(), str)
+            i += len(match.group())
+            yield from stat1()
+        else:
+            raise RuntimeError()
 
     if start < end and s[start] == quotation:
-
-
-        res = ''.join(stat1(start + 1))
-        #        print('start:%s,end:%s,res:%s' % (start, end, repr(res)))
-        return start + length, quotation + res + quotation
+        i = start + 1
+        res = tuple(stat1())
+        return start + length, res
     return start, ''
 
 
 def matchCharacterLiteral(s, start, end):
     res1, res2 = matchLiteralGeneral(s, start, end, '\'', Tag.charLiteral)
+    val = 0
+    for c in res2:
+        val = (val << 4) + ord(c)
+
     if len(res2) > 3:  # \', c, \'
         raise RuntimeError(r"multi-character character constant '%s'", res2)
     elif len(res2) == 2:
         raise RuntimeError("empty char constant")
-    return res1, res2
+    return res1, val
 
 
 def matchStringLiteral(s, start, end):
     return matchLiteralGeneral(s, start, end, '\"', Tag.stringLiteral)
+
+
+def matchGrammarStringLiteral(s, start, end):
+    a, b = matchLiteralGeneral(s, start, end, '\"', Tag.stringLiteral)
+    return a, ''.join(b)
+
+
+if __name__ == '__main__':
+    s = r"'0'"
+    f = lambda s: matchCharacterLiteral(s, 0, len(s))
+
+    print(f(s))
+    u = r'"\'hello\'\nworld!"'
+    g = lambda s: matchStringLiteral(s, 0, len(s))
+    print(g(u))
